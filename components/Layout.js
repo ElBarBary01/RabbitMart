2e2c12f2afb563df4d22e3e4a486cfa907cc2916
ElBarBary01 <93509515+ElBarBary01@users.noreply.github.com>
2022-06-12 23:59:15 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -22,7 +22,7 @@ function Layout({ children }) {
         <div className="px-20 py-3 bg-green-700">
           <div className="flex items-center justify-between">
             <div className="w-full text-red-700 md:text-left text-2xl font-semibold">
-            <a href="/"><img src="https://www.rabbitmart.com/wp-content/uploads/2021/11/Asset-4.png"style={{height: '3em', objectFit:'contain'}} width={130} alt="" /></a>
+            <Link href="/"><a><img src="https://www.rabbitmart.com/wp-content/uploads/2021/11/Asset-4.png"style={{height: '3em', objectFit:'contain'}} width={130} alt="" /></a></Link>
             </div>
             <div className="flex items-center justify-end w-full lg:w-2/5 lg:justify-around"> 
              <a href="/search"><button className="border p-1 mb-1 border-black w-2/9 shadow-offset-black font-bold rounded-full bg-yellow-300 hover:bg-yellow-200" >search</button></a>
